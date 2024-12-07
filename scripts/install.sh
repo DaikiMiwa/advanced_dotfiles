57f51fafdf6762748f69a06062f006d825b2b17d
Ubuntu <ubuntu@ip-172-31-46-181.ap-northeast-1.compute.internal>
2024-12-07 06:04:47 +0000

    feat: 🎸 [all] update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+chmod +x link.sh
+./link.sh
+
 # change shell from bash to zsh
 sudo apt install zsh
 chsh -s /bin/zsh
 
+sudo apt install zsh-completions
+sudo apt install zsh-autosuggestions
+
 # install neovim dependencies
-sudo apt install gcc ripgrep nodejs
+sudo apt install gcc ripgrep nodejs npm
 
 # install lazy git
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
@@ -15,14 +21,29 @@ sudo install lazygit -D -t /usr/local/bin/
 
 # install python through rye
 curl -sSf https://rye.astral.sh/get | bash
-echo 'source "$HOME/.rye/env"' >> ~/.zshrc
-source ~/.zshrc
 
 # Make sure ~/.zfunc is added to fpath, before compinit.
 rye self completion -s zsh > ~/.zfunc/_rye	
 
-# install neovim using snap
+# install neovim
 sudo apt install neovim 
 
-# install tmux using snap
+# install tmux
 sudo apt install tmux
+
+# github cli
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+
+# ghq
+sudo apt  install -y golang-go
+go install github.com/x-motemen/ghq@latest
+
+# git-cz
+npm install -g git-cz
+
--- scripts/link.sh
+++ scripts/link.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
+mkdir $HOME/.config
+
 # for neovim
 ln -s $HOME/.dotfiles/nvim $HOME/.config/nvim
 
 # for zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.config/.zshrc
-ln -s $HOME/.dotfiles/.zshenv $HOME/.config/.zshenv
-ln -s $HOME/.dotfiles/.zfunc $HOME/.config/.zfunc
+sudo wget -O /etc/zsh/zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
+wget -O $HOME/.zshrc https://git.grml.org/f/grml-etc-core/etc/skel/.zshrc
+ln -sv $HOME/.dotfiles/zsh/.zshrc.pre $HOME/.zshrc.pre
+
+# for tmux
+mkdir $HOME/.config/tmux
+ln -sv $HOME/.dotfiles/tmux/tmux.conf $HOME/.config/tmux/tmux.conf
+
+# for git-cz
+ln -sv $HOME/.dotfiles/git/.chanlog.config.js $HOME/.chanlog.config.js
+
+# for lazygit
+mkdir $HOME/.config/lazygit
+ln -sv $HOME/.dotfiles/git/lazygit_config.yaml $HOME/.config/lazygit/config.yml
