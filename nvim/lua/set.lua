5770b216bdc9298bce3619e80c0be9f344f90b48
DaikiMiwa <miwa.daiki.mllab.nit@gmail.com>
2025-01-20 00:58:54 +0900

    feat: 🎸 [nvim] add setting up option of nvim

---
+++ nvim/lua/set.lua
@@ -0,0 +1,7 @@
+-- word wrap
+vim.wo.wrap = true
+vim.wo.linebreak = true
+vim.wo.list = false -- extra option I set in addition to the ones in your question
+
+-- conceal level
+vim.o.conceallevel = 1
